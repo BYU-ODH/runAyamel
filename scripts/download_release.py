4f9c8a6570f250aaa980a8079760c246faad24a2
Santiago Verdu <santiago.verdu.01@gmail.com>
2019-01-28 19:38:41 -0700

    cleaner code i think

--- scripts/download_release.py
+++ scripts/download_release.py
@@ -2,12 +2,11 @@
 
 import requests
 import threading
-import sys
 import os.path
+import argparse
 
 base_url = 'https://github.com/BYU-ODH/'
 api_base_url = 'https://api.github.com/repos/BYU-ODH/%s/releases'
-production = True
 repos = [
     'yvideojs',
     'EditorWidgets',
@@ -17,9 +16,11 @@ repos = [
 ]
 
 class Worker(threading.Thread):
-    def __init__(self, repo):
+    def __init__(self, repo, authentication_query, production):
         threading.Thread.__init__(self)
         self.repo = repo
+        self.query_string = '?access_token=%s' % authentication_query if authentication_query is not None else ''
+        self.production = production
 
     def get_latest(self, releases):
         version = ''
@@ -28,7 +29,7 @@ class Worker(threading.Thread):
             # in production mode, prerelease must be false.
             # in staging, it doesn't matter what prerelease is, we just
             # want the latest release/prerelease
-            suitable_for_our_needs = ((production and rel['prerelease'] is False) or not production)
+            suitable_for_our_needs = ((self.production and rel['prerelease'] is False) or not self.production)
             if suitable_for_our_needs and len(rel['assets']) == 1 and rel['tag_name'] > version:
                 version = rel['tag_name']
                 latest = rel
@@ -47,7 +48,7 @@ class Worker(threading.Thread):
         return download_request
 
     def run(self):
-        release_response = requests.get(api_base_url % self.repo)
+        release_response = requests.get(api_base_url % self.repo + self.query_string)
         if release_response.status_code == 200:
             releases = release_response.json()
             latest = self.get_latest(releases)
@@ -60,20 +61,22 @@ class Worker(threading.Thread):
                         self.write_release(filename, stream)
                 print(filename)
 
+def parse_options():
+    parser = argparse.ArgumentParser(prog='download_release.py', description='Downloads the latest releases for yvideo dependencies', add_help=True)
+    parser.add_argument('-p', '--production', action='store_true', help='Whether to download production or beta code')
+    parser.add_argument('-a', '--access_token', action='store', help='token used for authorization')
+    return parser.parse_args()
+
 def download():
+    args = parse_options()
     workers = []
     for x in range(0, len(repos)):
-        workers.append(Worker(repos[x]))
+        workers.append(Worker(repos[x], args.access_token, args.production))
         workers[x].start()
 
     for x in range(0, len(repos)):
         workers[x].join()
 
-if len(sys.argv) > 1:
-    branch = sys.argv[1]
-    if branch != 'master':
-        production = True
-
 if __name__ == "__main__":
-    download(production)
+    download()
 
